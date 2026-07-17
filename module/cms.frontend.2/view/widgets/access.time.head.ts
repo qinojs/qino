96c7bf4c15c90671ff6d628553c3ed24aae91d00
Tobias Buschor <tobias.buschor@shwups.ch>
2026-07-17 06:20:32 +0200

    Inline online indicator

--- module/cms.frontend.2/view/widgets/access.time.head.ts
+++ module/cms.frontend.2/view/widgets/access.time.head.ts
@@ -3,7 +3,6 @@ import type { Node } from "../../../cms/mod.ts";
 
 export default async function (node: Node): Promise<HtmlString | string> {
   if ((await node.access()) < 2) return "";
-  const online = await node.isOnline();
-  const number = !online ? `<span class=-info>!</span>` : "";
+  const number = await node.isOnline() ? "" : `<span class=-info>!</span>`;
   return html.async`<span class=-title>${node.app.t`Schedule`}</span> ${html.raw(number)}`;
 }
